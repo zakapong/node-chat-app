92e7a9982aa088a95b9454718c99dddf9dcd7cac
Zakaria Mahmud <zaka.pong@yahoo.com>
2017-01-10 10:21:09 +0100

    init commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path= require('path');
+const express= require('express');
+
+const publicPath= path.join(__dirname, '../public');
+const port= process.env.PORT || 3000;
+var app= express();
+
+
+//console.log(__dirname+'/../public');
+//console.log();
+
+app.use(express.static(publicPath));
+
+app.listen(port, ()=>{
+  console.log(`Server is up on port ${port}`);
+});
