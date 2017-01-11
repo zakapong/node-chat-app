5c93b3038bff8371874258689088e6701604c7e1
Zakaria Mahmud <zaka.pong@yahoo.com>
2017-01-11 12:39:26 +0100

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -17,31 +17,32 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
   console.log('New user connected');
 
-  // socket.emit('newEmail', {
-  //   from: 'zaka.pong@yahoo.com',
-  //   text: 'Hey. What is going on.',
-  //   createAt: 123
-  // });
-
-  socket.emit('newMessage', {
-    from: 'Jhon@yahoo.com',
-    text: 'Hey. What is going on with new chat.',
-    createAt: 20170103
+   socket.emit('newMessage',{
+     from: 'Admin',
+     text: 'Welcome to the chat app!',
+       createAt: new Date().getTime()
+   });
+  socket.broadcast.emit('newMessage', {
+       from: 'Admin',
+       text: 'New user joined!',
+      createAt: new Date().getTime()
   });
 
- // socket.on('createEmail', (newEmail)=>{
- //   console.log('createEmail', newEmail);
- // });
+  socket.on('createMessage', (message)=>{
+    console.log('createMessage', message);
+  io.emit('createMessage',{
+    from: message.from,
+    text: message.text,
+    createAt: new Date().getTime()
+  })
+});
 
- socket.on('createMessage', (message)=>{
-   console.log('createMessage', message);
- });
 
   socket.on('disconnect', ()=>{
     console.log('User was disconnected');
   });
-});
 
+ });
 server.listen(port, ()=>{
   console.log(`Server is up on port ${port}`);
 });
