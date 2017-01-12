c05b757382d370656cfa20a94dc10153355f4c53
Zakaria Mahmud <zaka.pong@yahoo.com>
2017-01-12 20:06:34 +0100

    Formatted timstamps using momentjs

---
+++ playground/time.js
@@ -0,0 +1,19 @@
+var moment=require('moment');
+
+// Jan 1st 1970 00:00:10 am
+
+//var date=new Date();
+//console.log(date.getMonth());
+
+
+var date=moment();
+//date.add(100, 'year').subtract(9, 'months');
+//console.log(date.format('MMM Do, YYYY'));
+
+var someTimestamp= moment().valueOf();
+console.log(someTimestamp);
+
+
+var createAt=1234;
+var date=moment(createAt);
+console.log(date.format('h:mm a'));
